7c9835be4739612d5d5eb88083e7911140ed1a38
毛庭峰 <mtingfeng@gmail.com>
2016-11-16 11:21:18 +0800

    websocket连接成功

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-var express = require('express') ,
-    path = require( 'path' ) ,
-    websocket = require( './websocket' ) ,
-    _static_path = path.join( __dirname , 'public' ) ,
-    app = express() ;
-function log( txt ){
-    console.log( 'wbfresh程序：' , txt ) ;
-}
-
-app.use( express.static( _static_path ) );
+var server = require('http').createServer()
+  , url = require('url')
+  , WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer( {
+        server: server ,
+        verifyClient: function( info ){
+            return true ;
+        }
+    } )
+  , express = require('express')
+  , app = express()
+  , port = 4080
+  , log = function( txt ){ console.log( 'wbfresh' , txt ) ; } ;
 
 app.set( 'view engine' , 'pug' ) ;
 app.set( 'views' , './views' ) ;
 
-app.get( '/' , function (req, res) {
+app.use( express.static( './public' ) ) ;
+
+app.use( function (req, res) {
     res.render( 'index' ) ;
 } ) ;
 
-websocket( app ) ;
-app.listen( 3000 , function () {
-    log('开始监听3000端口') ;
+wss.on('connection', function connection( ws ){
+    var location = url.parse( ws.upgradeReq.url , true ) ;
+    // you might use location.query.access_token to authenticate or share sessions
+    // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+    ws.on('message', function incoming(message){
+        console.log('received: %s', message);
+    } ) ;
+    //ws.send( 'refresh' ) ;
+
 } ) ;
 
+server.on( 'request', app ) ;
+server.listen( port , function () {
+    log( server.address().port ) ;
+} );
--- public/wbfresh.js
+++ public/wbfresh.js
@@ -6,26 +6,42 @@
  * @date     2016-11-10 09:15:43
  * --------------------------------------------
  */
-function connect(){
-    var host = location.host ,
+function wbfresh_connect(){
+    var slice = Array.prototype.slice ,
+        host = location.host ,
         url = 'ws://' + host ,                                  // 形如:'ws://192.168.1.105/'
         connection = new WebSocket( url , 'echo-protocol' ) ;
     connection.onopen = wsOpen ;
     connection.onclose = wsClose ;
     connection.onmessage = wsMessage ;
     connection.onerror = wsError ;
+    function warn(){
+        var args = slice.call( arguments ) ;
+        args.unshift( 'wbfresh.js' ) ;
+        console.warn.apply( null , args ) ;
+    }
+    function log(){
+        var args = slice.call( arguments ) ;
+        args.unshift( 'wbfresh.js' ) ;
+        console.log.apply( null , args ) ;
+    }
     function wsOpen( event ){
-        console.log( '连接到:' + event.currentTarget.url ) ;
+        log( '连接到:' + event.currentTarget.url ) ;
         //connection.send( 'socket111.html' ) ;
     }
     function wsClose(){
-        console.log( '连接关闭' ) ;
+        warn( '连接关闭' ) ;
     }
     function wsMessage( event ){
-        console.log( '接收到数据:' + event.data );
+        var _data = event.data ;
+        if( _data == 'refresh' ){
+            location.reload() ;
+        }else{
+            log( '接收到数据:' + event.data );
+        }
     }
     function wsError( event ){
-        console.log( '错误:' + event.data ) ;
+        warn( event.data ) ;
     }
 }
-setTimeout( connect , 0 ) ;
+setTimeout( wbfresh_connect , 0 ) ;
